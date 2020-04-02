92d8ad70fdfcd3080ccadf9cad8a7a5e4bd27c34
Kathy <jackathy@htva.net>
2020-04-02 15:51:35 -0400

    End of lesson 34. All works except formatting of "product" page isn't exactly right.

--- src/main/java/guru/springframework/springmvc/controllers/ProductController.java
+++ src/main/java/guru/springframework/springmvc/controllers/ProductController.java
@@ -5,6 +5,7 @@ import guru.springframework.springmvc.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -24,5 +25,13 @@ public class ProductController {
         model.addAttribute("products", productService.listAllProducts());
         
         return "products"; // This must equal the html file name (products.html)
+    }
+
+    @RequestMapping("/product/{id}")
+    public String getProduct(@PathVariable Integer id, Model model){
+
+        model.addAttribute("product", productService.getProductById(id));
+
+        return  "product";
     }
 }
