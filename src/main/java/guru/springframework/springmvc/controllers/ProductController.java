47aca1c8b1164755efd99c8ec4af669978db4a5e
Kathy <jackathy@htva.net>
2020-04-02 17:26:42 -0400

    End of lesson 34 - creates a new product.

--- src/main/java/guru/springframework/springmvc/controllers/ProductController.java
+++ src/main/java/guru/springframework/springmvc/controllers/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class ProductController {
@@ -29,9 +30,22 @@ public class ProductController {
 
     @RequestMapping("/product/{id}")
     public String getProduct(@PathVariable Integer id, Model model){
-
         model.addAttribute("product", productService.getProductById(id));
 
         return  "product";
+    }
+
+    @RequestMapping("/product/new")
+    public String newProduct(Model model){
+        model.addAttribute("product", new Product());
+
+        return "productform";
+    }
+
+    @RequestMapping(value = "/product", method = RequestMethod.POST)
+    public String saveOrUpdateProduct(Product product){
+        Product savedProduct = productService.saveOrUpdateProduct(product);
+
+        return "redirect:/product/" + savedProduct.getId();
     }
 }
